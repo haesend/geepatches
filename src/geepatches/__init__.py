ab74b22b859a4d11803fa6e0d94c25539313b11c
HAESEND <dominique.haesen@vito.be>
2022-03-10 08:05:10 +0100

    handy

--- src/geepatches/__init__.py
+++ src/geepatches/__init__.py
@@ -1,5 +1,9 @@
+import sys
+print(f"sys.version({sys.version})")
 import ee
 if not ee.data._credentials: ee.Initialize()
 print (f"ee.__version__({ee.__version__})")
 import geemap
 print (f"geemap.__version__({geemap.__version__})")
+import osgeo.gdal
+print (f"osgeo.gdal.VersionInfo()({osgeo.gdal.VersionInfo()})")
